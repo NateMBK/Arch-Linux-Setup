d87a6df72888b95d7ac74f9571bfa9ccd0dd259f
Nate <gtanatembk@hotmail.com>
2023-02-26 17:11:31 -0800

    undo changes

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -43,6 +43,16 @@ root_check() {
     fi
 }
 
+docker_check() {
+    if awk -F/ '$2 == "docker"' /proc/self/cgroup | read -r; then
+        echo -ne "ERROR! Docker container is not supported (at the moment)\n"
+        exit 0
+    elif [[ -f /.dockerenv ]]; then
+        echo -ne "ERROR! Docker container is not supported (at the moment)\n"
+        exit 0
+    fi
+}
+
 arch_check() {
     if [[ ! -e /etc/arch-release ]]; then
         echo -ne "ERROR! This script must be run in Arch Linux!\n"
@@ -62,6 +72,7 @@ background_checks() {
     root_check
     arch_check
     pacman_check
+    docker_check
 }
 
 # Renders a text based list of options that can be selected by the
@@ -173,20 +184,120 @@ select_option() {
 
     return $(( $active_col + $active_row * $colmax ))
 }
+# @description Displays ArchTitus logo
+# @noargs
+logo () {
+# This will be shown on every set as user is progressing
+echo -ne "
+-------------------------------------------------------------------------
+ █████╗ ██████╗  ██████╗██╗  ██╗████████╗██╗████████╗██╗   ██╗███████╗
+██╔══██╗██╔══██╗██╔════╝██║  ██║╚══██╔══╝██║╚══██╔══╝██║   ██║██╔════╝
+███████║██████╔╝██║     ███████║   ██║   ██║   ██║   ██║   ██║███████╗
+██╔══██║██╔══██╗██║     ██╔══██║   ██║   ██║   ██║   ██║   ██║╚════██║
+██║  ██║██║  ██║╚██████╗██║  ██║   ██║   ██║   ██║   ╚██████╔╝███████║
+╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝   ╚═╝   ╚═╝   ╚═╝    ╚═════╝ ╚══════╝
+------------------------------------------------------------------------
+            Please select presetup settings for your system              
+------------------------------------------------------------------------
+"
+}
 # @description This function will handle file systems. At this movement we are handling only
 # btrfs and ext4. Others will be added in future.
 filesystem () {
-set_option FS btrfs;;
-}
+echo -ne "
+Please Select your file system for both boot and root
+"
+options=("btrfs" "ext4" "luks" "exit")
+select_option $? 1 "${options[@]}"
 
+case $? in
+0) set_option FS btrfs;;
+1) set_option FS ext4;;
+2) 
+    set_password "LUKS_PASSWORD"
+    set_option FS luks
+    ;;
+3) exit ;;
+*) echo "Wrong option please select again"; filesystem;;
+esac
+}
 # @description Detects and sets timezone. 
 timezone () {
-set_option TIMEZONE America/Los_Angeles;;
-}
+# Added this from arch wiki https://wiki.archlinux.org/title/System_time
+time_zone="$(curl --fail https://ipapi.co/timezone)"
+echo -ne "
+System detected your timezone to be '$time_zone' \n"
+echo -ne "Is this correct?
+" 
+options=("Yes" "No")
+select_option $? 1 "${options[@]}"
 
+case ${options[$?]} in
+    y|Y|yes|Yes|YES)
+    echo "${time_zone} set as timezone"
+    set_option TIMEZONE $time_zone;;
+    n|N|no|NO|No)
+    echo "Please enter your desired timezone e.g. Europe/London :" 
+    read new_timezone
+    echo "${new_timezone} set as timezone"
+    set_option TIMEZONE $new_timezone;;
+    *) echo "Wrong option. Try again";timezone;;
+esac
+}
 # @description Set user's keyboard mapping. 
 keymap () {
-set_option KEYMAP $us
+echo -ne "
+Please select key board layout from this list"
+# These are default key maps as presented in official arch repo archinstall
+options=(us by ca cf cz de dk es et fa fi fr gr hu il it lt lv mk nl no pl ro ru sg ua uk)
+
+select_option $? 4 "${options[@]}"
+keymap=${options[$?]}
+
+echo -ne "Your key boards layout: ${keymap} \n"
+set_option KEYMAP $keymap
+}
+
+# @description Choose whether drive is SSD or not.
+drivessd () {
+echo -ne "
+Is this an ssd? yes/no:
+"
+
+options=("Yes" "No")
+select_option $? 1 "${options[@]}"
+
+case ${options[$?]} in
+    y|Y|yes|Yes|YES)
+    set_option MOUNT_OPTIONS "noatime,compress=zstd,ssd,commit=120";;
+    n|N|no|NO|No)
+    set_option MOUNT_OPTIONS "noatime,compress=zstd,commit=120";;
+    *) echo "Wrong option. Try again";drivessd;;
+esac
+}
+
+# @description Disk selection for drive to be used with installation.
+diskpart () {
+echo -ne "
+------------------------------------------------------------------------
+    THIS WILL FORMAT AND DELETE ALL DATA ON THE DISK
+    Please make sure you know what you are doing because
+    after formating your disk there is no way to get data back
+------------------------------------------------------------------------
+
+"
+
+PS3='
+Select the disk to install on: '
+options=($(lsblk -n --output TYPE,KNAME,SIZE | awk '$1=="disk"{print "/dev/"$2"|"$3}'))
+
+select_option $? 1 "${options[@]}"
+disk=${options[$?]%|*}
+
+echo -e "\n${disk%|*} selected \n"
+    set_option DISK ${disk%|*}
+
+drivessd
 }
 
 # @description Gather username and password to be used for installation. 
@@ -200,7 +311,12 @@ set_option NAME_OF_MACHINE $nameofmachine
 
 # @description Choose AUR helper. 
 aurhelper () {
-  set_option AUR_HELPER yay
+  # Let the user choose AUR helper from predefined list
+  echo -ne "Please enter your desired AUR helper:\n"
+  options=(paru yay picaur aura trizen pacaur none)
+  select_option $? 4 "${options[@]}"
+  aur_helper=${options[$?]}
+  set_option AUR_HELPER $aur_helper
 }
 
 # @description Choose Desktop Environment
@@ -213,6 +329,20 @@ desktopenv () {
   set_option DESKTOP_ENV $desktop_env
 }
 
+# @description Choose whether to do full or minimal installation. 
+installtype () {
+  echo -ne "Please select type of installation:\n\n
+  Full install: Installs full featured desktop enviroment, with added apps and themes needed for everyday use\n
+  Minimal Install: Installs only apps few selected apps to get you started\n"
+  options=(FULL MINIMAL)
+  select_option $? 4 "${options[@]}"
+  install_type=${options[$?]}
+  set_option INSTALL_TYPE $install_type
+}
+
+# More features in future
+# language (){}
+
 # Starting functions
 background_checks
 clear
@@ -222,11 +352,25 @@ clear
 logo
 desktopenv
 # Set fixed options that installation uses if user choses server installation
+set_option INSTALL_TYPE MINIMAL
+set_option AUR_HELPER NONE
+if [[ ! $desktop_env == server ]]; then
+  clear
+  logo
+  aurhelper
+  clear
+  logo
+  installtype
+fi
 clear
+logo
 diskpart
 clear
+logo
 filesystem
 clear
+logo
 timezone
 clear
+logo
 keymap
