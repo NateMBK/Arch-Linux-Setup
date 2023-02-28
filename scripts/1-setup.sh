012382de40fbe9c90a1aa266a48322c48410b076
Nate <gtanatembk@hotmail.com>
2023-02-27 21:43:28 -0800

    -

--- scripts/1-setup.sh
+++ scripts/1-setup.sh
@@ -70,9 +70,18 @@ echo -ne "
                     Installing Base System  
 -------------------------------------------------------------------------
 "
-sed -n '/'$INSTALL_TYPE'/q;p' $HOME/ArchTitus/pkg-files/pacman-pkgs.txt
-sudo pacman -S --noconfirm --needed ${line}
-done
+# sed $INSTALL_TYPE is using install type to check for MINIMAL installation, if it's true, stop
+# stop the script and move on, not installing any more packages below that line
+if [[ ! $DESKTOP_ENV == server ]]; then
+  sed -n '/'$INSTALL_TYPE'/q;p' $HOME/ArchTitus/pkg-files/pacman-pkgs.txt | while read line
+  do
+    if [[ ${line} == '--END OF MINIMAL INSTALL--' ]]; then
+      # If selected installation type is FULL, skip the --END OF THE MINIMAL INSTALLATION-- line
+      continue
+    fi
+    echo "INSTALLING: ${line}"
+    sudo pacman -S --noconfirm --needed ${line}
+  done
 fi
 echo -ne "
 -------------------------------------------------------------------------
