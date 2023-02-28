9588d64f484e0d7fd50743b4fbf8360fd4185e1f
Nate <gtanatembk@hotmail.com>
2023-02-27 22:03:59 -0800

    Update 2-user.sh

--- scripts/2-user.sh
+++ scripts/2-user.sh
@@ -47,19 +47,19 @@ fi
 export PATH=$PATH:~/.local/bin
 
 #Theming DE if user chose FULL installation
-if [[ $INSTALL_TYPE == "FULL" ]]; then
-  if [[ $DESKTOP_ENV == "kde" ]]; then
-    cp -r ~/ArchTitus/configs/.config/* ~/.config/
-    pip install konsave
-    konsave -i ~/ArchTitus/configs/kde.knsv
-    sleep 1
-    konsave -a kde
-  elif [[ $DESKTOP_ENV == "openbox" ]]; then
-    cd ~
-    git clone https://github.com/stojshic/dotfiles-openbox
-    ./dotfiles-openbox/install-titus.sh
-  fi
-fi
+#if [[ $INSTALL_TYPE == "FULL" ]]; then
+#  if [[ $DESKTOP_ENV == "kde" ]]; then
+#    cp -r ~/ArchTitus/configs/.config/* ~/.config/
+#    pip install konsave
+#    konsave -i ~/ArchTitus/configs/kde.knsv
+#    sleep 1
+#    konsave -a kde
+#  elif [[ $DESKTOP_ENV == "openbox" ]]; then
+#    cd ~
+#    git clone https://github.com/stojshic/dotfiles-openbox
+#    ./dotfiles-openbox/install-titus.sh
+#  fi
+#fi
 
 echo -ne "
 -------------------------------------------------------------------------
