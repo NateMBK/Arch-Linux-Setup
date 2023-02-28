0118ab61d8e7daa67bcd4ee8a882ff7002adc0db
Nate <gtanatembk@hotmail.com>
2023-02-27 21:33:38 -0800

    -

--- scripts/3-post-setup.sh
+++ scripts/3-post-setup.sh
@@ -68,7 +68,7 @@ echo "  Bluetooth enabled"
 systemctl enable avahi-daemon.service
 echo "  Avahi enabled"
 
-if [[ "${FS}" == "luks" || "${FS}" == "btrfs" ]]; then
+if [[ "${FS}" == "btrfs" ]]; then
 echo -ne "
 -------------------------------------------------------------------------
                     Creating Snapper Config
