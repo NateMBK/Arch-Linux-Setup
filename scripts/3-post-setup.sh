912d7892fde32f6696d3f09d5ea90ef46a78a05a
Nate <gtanatembk@hotmail.com>
2023-02-28 22:03:58 -0800

    fix them install syntax error

--- scripts/3-post-setup.sh
+++ scripts/3-post-setup.sh
@@ -83,14 +83,15 @@ echo -ne "
                Enabling (and Theming) Plymouth Boot Splash
 -------------------------------------------------------------------------
 "
-PLYMOUTH_THEMES_DIR="$HOME/ArchTitus/configs/usr/share/plymouth/themes"
-PLYMOUTH_THEME="arch-glow" # can grab from config later if we allow selection
-mkdir -p /usr/share/plymouth/themes
-echo 'Installing Plymouth theme...'
-cp -rf ${PLYMOUTH_THEMES_DIR}/${PLYMOUTH_THEME} /usr/share/plymouth/themes
-sed -i 's/HOOKS=(base udev*/& plymouth/' /etc/mkinitcpio.conf # add plymouth after base udev
-plymouth-set-default-theme -R arch-glow # sets the theme and runs mkinitcpio
-echo 'Plymouth theme installed'
+if [[ "${FS}" == "btrfs" ]]; then
+  PLYMOUTH_THEMES_DIR="$HOME/ArchTitus/configs/usr/share/plymouth/themes"
+  PLYMOUTH_THEME="arch-glow" # can grab from config later if we allow selection
+  mkdir -p /usr/share/plymouth/themes
+  echo 'Installing Plymouth theme...'
+  cp -rf ${PLYMOUTH_THEMES_DIR}/${PLYMOUTH_THEME} /usr/share/plymouth/themes
+  sed -i 's/HOOKS=(base udev*/& plymouth/' /etc/mkinitcpio.conf # add plymouth after base udev
+  plymouth-set-default-theme -R arch-glow # sets the theme and runs mkinitcpio
+  echo 'Plymouth theme installed'
 fi
 
 echo -ne "
