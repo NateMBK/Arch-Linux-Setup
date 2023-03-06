bd54e3a841dde3cd76ffc91184dba54da2232c45
Nate <gtanatembk@hotmail.com>
2023-03-05 22:21:33 -0800

    -

--- scripts/2-user.sh
+++ scripts/2-user.sh
@@ -2,32 +2,33 @@
 # @file User
 # @brief User customizations and AUR package installation.
 
-source $HOME/ArchTitus/configs/setup.conf
-cd ~
-sed -n 's/^\s*#.*//; /^[[:space:]]*$/d; p' ~/ArchTitus/pkg-files/kde.txt | while read line
+source "$HOME/ArchTitus/configs/setup.conf"
+
+cd "$HOME"
+sed -n 's/^\s*#.*//; /^[[:space:]]*$/d; p' "$HOME/ArchTitus/pkg-files/kde.txt" | while read line
 do
     sudo pacman -S --noconfirm --needed "${line}"
 done
 
-cd ~
+cd "$HOME"
 git clone "https://aur.archlinux.org/yay.git"
-cd ~/yay
+cd "$HOME/yay"
 makepkg -si --noconfirm
-sed -n 's/^\s*#.*//; /^[[:space:]]*$/d; p' ~/ArchTitus/pkg-files/aur-pkgs.txt | while read line
+sed -n 's/^\s*#.*//; /^[[:space:]]*$/d; p' "$HOME/ArchTitus/pkg-files/aur-pkgs.txt" | while read line
 do
     yay -S --noconfirm --needed "${line}"
 done
 
-export PATH=$PATH:~/.local/bin
+export PATH="$PATH:$HOME/.local/bin"
 
-#Theming DE if user chose FULL installation
-#cp -r ~/ArchTitus/configs/.config/* ~/.config/
-#pip install konsave
-#konsave -i ~/ArchTitus/configs/kde.knsv
-#sleep 1
-#konsave -a kde
+# Theming DE if user chose FULL installation
+# cp -r "$HOME/ArchTitus/configs/.config/*" "$HOME/.config/"
+# pip install konsave
+# konsave -i "$HOME/ArchTitus/configs/kde.knsv"
+# sleep 1
+# konsave -a kde
 
-echo -ne "___________________________________
+echo -e "___________________________________
 
 Starting 3-post-setup
 
